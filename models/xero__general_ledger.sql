18bb27d6ef9230bb9e8276b0fcd9254569287a1a
Paula Videla <paulavidela09@gmail.com>
2022-07-18 23:41:11 -0300

    feat: add tracking category

--- models/xero__general_ledger.sql
+++ models/xero__general_ledger.sql
@@ -1,3 +1,41 @@
+/*
+in order to generate trancking_category_name as a column we set a dictionary
+dictionary has the following appareance{'tracking_category_id' : [tuple of available id], 'tracking_category_name': [tuple of available names]}
+position match for id and name since the dictionary was generated with a sql statement 
+*/
+
+{% set tracking_category_sql %}
+    select 
+        distinct tracking_category_id,
+        regexp_replace(lower(name), r'\W', '_')   as tracking_category_name
+    from {{var('tracking_category')}}
+    where status = 'ACTIVE'
+{% endset %}
+
+
+
+{% if execute %}
+    -- fivetran_utils depends on dbt_utils 
+    {% set tc_dict = dbt_utils.get_query_results_as_dict(tracking_category_sql) %}
+    {% set tc_length = tc_dict['tracking_category_id']|length %}
+
+    -- since this case when is repetead for all source_type, we set it here as a macro and call it each time it is needed
+    {% set case_when_tracking_category %}
+        {% for n in range(0, tc_length )%}
+            case when tracking_category_id = '{{tc_dict['tracking_category_id'][n]}}'
+                -- we concate available options for that tracking_category_id 
+                then string_agg(distinct option, ' | ') 
+                    end as {{tc_dict['tracking_category_name'][n]}}  {% if not loop.last %} , {% endif %}
+
+        {% endfor %} 
+    {% endset %}
+
+{% else %}
+    {% set tc_length = 1 %}
+    {% set case_when_tracking_category = '' %} 
+{% endif%}
+
+
 with journals as (
 
     select *
@@ -13,17 +51,20 @@ with journals as (
         j.account_name,
         j.account_type,
         jt.option,
+        jt.tracking_category_id,
         j.description,
         j.gross_amount,
         j.journal_id,
         j.net_amount,
         j.tax_amount,
         j.tax_name,
-        j.tax_type
+        j.tax_type,
+        {{case_when_tracking_category}}
+
     from {{ var('journal_line') }} j
     left join {{  var('journal_line_tracking') }} jt on
         j.journal_line_id = jt.journal_line_id
-    {{ dbt_utils.group_by(14) }}
+    {{ dbt_utils.group_by(15) }}
 
 ), accounts as (
 
@@ -51,11 +92,14 @@ with journals as (
         i.invoice_status,
         i.type,
         i.url,
-        it.option
+        it.option,
+        it.tracking_category_id,
+        {{case_when_tracking_category}}
+
     from {{ var('invoice') }} i
     left join {{ var('invoice_line_item_tracking') }} it on
         i.invoice_id = it.invoice_id
-    {{ dbt_utils.group_by(19) }}
+    {{ dbt_utils.group_by(20) }}
 
 {% if var('xero__using_bank_transaction', True) %}
 ), bank_transactions as (
@@ -64,11 +108,13 @@ with journals as (
         b.bank_transaction_id,
         b.source_relation,
         b.contact_id,
-        bt.option
+        bt.option,
+        bt.tracking_category_id,
+        {{case_when_tracking_category}}
     from {{ var('bank_transaction') }} b
     left join {{ var('bank_transaction_tracking') }} bt on
         b.bank_transaction_id = bt.bank_transaction_id
-    {{ dbt_utils.group_by(4) }}
+    {{ dbt_utils.group_by(5) }}
 
 
 {% endif %}
@@ -80,11 +126,13 @@ with journals as (
         c.credit_note_id,
         c.source_relation,
         c.contact_id, 
-        ct.option
+        ct.option,
+        ct.tracking_category_id,
+        {{case_when_tracking_category}}
     from {{ var('credit_note') }} c
     left join {{ var('credit_note_tracking') }} ct on
         c.credit_note_id = ct.credit_note_id
-    {{ dbt_utils.group_by(4) }}
+    {{ dbt_utils.group_by(5) }}
 
 {% endif %}
 
@@ -111,6 +159,9 @@ with journals as (
         accounts.account_type,
         journal_lines.description,
         journal_lines.option,
+        {% for n in range(0, tc_length )%}
+        journal_lines.{{tc_dict['tracking_category_name'][n]}} , 
+        {% endfor %}
         journal_lines.gross_amount,
         journal_lines.net_amount,
         journal_lines.tax_amount,
@@ -135,8 +186,36 @@ with journals as (
 ), first_contact as (
 
     select 
-        joined.*,
+        joined.journal_id,
+        joined.created_date_utc,
+        joined.journal_date,
+        joined.journal_number,
+        joined.reference,
+        joined.source_id,
+        joined.source_type,
+        joined.source_relation,
+        joined.journal_line_id,
+        joined.account_code,
+        joined.account_id,
+        joined.account_name,
+        joined.account_type,
+        joined.description,
+        joined.option,
+        joined.gross_amount,
+        joined.net_amount,
+        joined.tax_amount,
+        joined.tax_name,
+        joined.tax_type,
+        joined.account_class,
+        joined.invoice_id,
+        joined.bank_transaction_id,
+        joined.bank_transfer_id,
+        joined.manual_journal_id,
+        joined.payment_id,
+        joined.credit_note_id,
+
         {% if fivetran_utils.enabled_vars_one_true(['xero__using_bank_transaction','xero__using_credit_note']) %}
+        
         coalesce(
             invoices.contact_id
             {% if var('xero__using_bank_transaction', True) %}
@@ -151,21 +230,24 @@ with journals as (
         invoices.contact_id
         {% endif %}
 
-        as contact_id,
+        as contact_id
 
-        coalesce(
-            joined.option
-            ,invoices.option
+        {% for n in range(0, tc_length )%}
+        ,coalesce(
+            joined.{{tc_dict['tracking_category_name'][n]}}, 
+            invoices.{{tc_dict['tracking_category_name'][n]}}
+
             {% if var('xero__using_bank_transaction', True) %}
-                , bank_transactions.option
+            , bank_transactions.{{tc_dict['tracking_category_name'][n]}}
             {% endif %}
 
             {% if var('xero__using_credit_note', True) %}
-                , credit_notes.option
+            , credit_notes.{{tc_dict['tracking_category_name'][n]}}
             {% endif %}
-        )
 
-        as full_option,
+            ) as {{tc_dict['tracking_category_name'][n]}}
+        {% endfor %}
+
 
     from joined
     left join invoices 
@@ -182,7 +264,7 @@ with journals as (
         on (joined.credit_note_id = credit_notes.credit_note_id
         and joined.source_relation = credit_notes.source_relation)
     {% endif %}
-    {{ dbt_utils.group_by(29) }}
+    {{ dbt_utils.group_by(28 + tc_length) }}
 
 ), second_contact as (
 
--- models/xero__profit_and_loss_report.sql
+++ models/xero__profit_and_loss_report.sql
@@ -1,3 +1,27 @@
+
+/*
+we get the distinct tacking_category_name available
+*/
+
+{% set tracking_category_sql %}
+    select 
+        distinct tracking_category_id,
+        regexp_replace(lower(name), r'\W', '_')   as tracking_category_name
+    from {{var('tracking_category')}}
+    where status = 'ACTIVE'
+{% endset %}
+
+{% if execute %}
+    {% set tracking_category = run_query(tracking_category_sql) %}
+    {% set tracking_category_list = tracking_category.columns[1].values() %}
+    {% set tc_length = tracking_category_list|length %}
+
+{% else %}
+    {% set tc_length = 1 %}
+    {% set tracking_category_list = [] %} 
+
+{% endif%}
+
 with calendar as (
 
     select *
@@ -18,14 +42,16 @@ with calendar as (
         ledger.account_code,
         ledger.account_type, 
         ledger.account_class,
-        ledger.full_option,
+        {% for n in range(0, tc_length )%}
+        {{tracking_category_list[n]}} , 
+        {% endfor %}
         ledger.source_relation, 
         coalesce(sum(ledger.net_amount * -1),0) as net_amount
     from calendar
     left join ledger
         on calendar.date_month = cast({{ dbt_utils.date_trunc('month', 'ledger.journal_date') }} as date)
     where ledger.account_class in ('REVENUE','EXPENSE')
-    {{ dbt_utils.group_by(9) }}
+    {{ dbt_utils.group_by(8 + tc_length) }}
 
 )
 
